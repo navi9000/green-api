0db5be086f59c92da914a9e5da79711d634bc354
navi9000 <icdi7434@gmail.com>
2026-09-17 21:05:22 +0400

    home page sections, copy background from max

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,7 +1,19 @@
 import type { FC } from "react"
+import styles from "./HomePage.module.css"
+import clsx from "clsx"
 
 const HomePage: FC = () => {
-  return <div>Home Page</div>
+  return (
+    <div className={styles.page}>
+      <div className={styles.background}>
+        <div className={clsx(styles.layer, styles.layerBase)} />
+        <div className={clsx(styles.layer, styles.layerAdditional)} />
+        <div className={clsx(styles.layer, styles.layerPattern)} />
+      </div>
+      <aside className={styles.chatlistcontainer}></aside>
+      <div className={styles.chatcontainer}></div>
+    </div>
+  )
 }
 
 export default HomePage
